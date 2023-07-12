44e3902af8e89b06a4be836a6a5d3c42b1bade5f
eluCve <71392454+eluCve@users.noreply.github.com>
2023-07-12 11:18:38 +0300

    Update mapInit.js

--- mapInit.js
+++ mapInit.js
@@ -56,9 +56,9 @@ function initMap() {
   });
   map.data.addListener("click", function (event) {
     console.log(event)
-  //  let areaName = event.feature.j.name;
-   // let areaURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=${areaName}+drone+view&key=AIzaSyC1diT_AA4nMwnZl_6LTr0hAO0t-gAKTrc`;
-  //  searchArea(areaURL, areaName);
+    let areaName = event.feature.h.name;
+    let areaURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=${areaName}+drone+view&key=AIzaSyC1diT_AA4nMwnZl_6LTr0hAO0t-gAKTrc`;
+    searchArea(areaURL, areaName);
   });
   let mapErrorAPI = setInterval(() => {
     let mapError = document.querySelectorAll("#map > div");
